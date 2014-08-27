d1410c2b0c54325793cc696b5ba7375143f4cd3a
coldAmphibian <coldamphibian@gmail.com>
2014-08-27 23:04:33 +1000

    Started work on bushes

--- scripts/vscripts/summoners_rift_scripts.lua
+++ scripts/vscripts/summoners_rift_scripts.lua
@@ -1,23 +1,25 @@
 function AntiBlockStart(trigger)
 	if trigger.activator then
 		trigger.activator:AddNewModifier(trigger.activator, nil, "modifier_phased", nil)
-		end
 	end
+end
 
 function AntiBlockEnd(trigger)
 	if trigger.activator then
 		trigger.activator:RemoveModifierByName("modifier_phased")
-		end
 	end
+end
 
 function BushStart(trigger)
 	if trigger.activator then
 		trigger.activator:AddNewModifier(trigger.activator, nil, "modifier_invisible", nil)
-		end
+		trigger.activator:AddNewModifier(trigger.activator, nil, "modifier_truesight", nil)
 	end
+end
 
 function BushEnd(trigger)
 	if trigger.activator then
 		trigger.activator:RemoveModifierByName("modifier_invisible")
-		end
+		trigger.activator:RemoveModifierByName("modifier_truesight")
 	end
+end
