f195e50e860a10a9f41fdd59d0c6d435831cb48a
coldAmphibian <coldamphibian@gmail.com>
2014-08-31 11:31:00 +1000

    Tried Creating baron board, played around with bushes

--- scripts/vscripts/summoners_rift_scripts.lua
+++ scripts/vscripts/summoners_rift_scripts.lua
@@ -1,25 +1,26 @@
 function AntiBlockStart(trigger)
 	if trigger.activator then
-		trigger.activator:AddNewModifier(trigger.activator, nil, "modifier_phased", nil)
+		trigger.activator:AddNewModifier(trigger.activator, nil, 'modifier_phased', nil)
 	end
 end
 
 function AntiBlockEnd(trigger)
 	if trigger.activator then
-		trigger.activator:RemoveModifierByName("modifier_phased")
+		trigger.activator:RemoveModifierByName('modifier_phased')
 	end
 end
 
 function BushStart(trigger)
 	if trigger.activator then
-		trigger.activator:AddNewModifier(trigger.activator, nil, "modifier_invisible", nil)
-		trigger.activator:AddNewModifier(trigger.activator, nil, "modifier_truesight", nil)
+		trigger.activator:AddNewModifier(trigger.activator, nil, 'modifier_invisible', nil)
+		trigger.activator:AddNewModifier(trigger.activator, nil, 'modifier_truesight', nil)
+		trigger.activator:AddNewModifier(trigger.activator, nil, 'modifier_item_heart', nil)
 	end
 end
 
 function BushEnd(trigger)
 	if trigger.activator then
-		trigger.activator:RemoveModifierByName("modifier_invisible")
-		trigger.activator:RemoveModifierByName("modifier_truesight")
+		trigger.activator:RemoveModifierByName('modifier_invisible')
+		trigger.activator:RemoveModifierByName('modifier_truesight')
 	end
 end
