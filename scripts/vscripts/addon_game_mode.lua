c3c79920eae981073fd5f449ddd9a41c879db11d
coldAmphibian <coldamphibian@gmail.com>
2014-09-04 10:03:36 +1000

    Cleaning up and redoing some textures, started adding 3d models

--- scripts/vscripts/addon_game_mode.lua
+++ scripts/vscripts/addon_game_mode.lua
@@ -1,40 +1,36 @@
---[[
-Dota PvP game mode
-]]
+-- Generated from template
 
-require("GameData")
-require("GameDataInfo")
-
-print( "Dota PvP game mode loaded." )
-
-if DotaPvP == nil then
-	DotaPvP = class({})
+if CAddonTemplateGameMode == nil then
+	CAddonTemplateGameMode = class({})
 end
 
---------------------------------------------------------------------------------
--- ACTIVATE
---------------------------------------------------------------------------------
+function Precache( context )
+	--[[
+		Precache things we know we'll use.  Possible file types include (but not limited to):
+			PrecacheResource( "model", "*.vmdl", context )
+			PrecacheResource( "soundfile", "*.vsndevts", context )
+			PrecacheResource( "particle", "*.vpcf", context )
+			PrecacheResource( "particle_folder", "particles/folder", context )
+	]]
+end
+
+-- Create the game mode when we activate
 function Activate()
-    GameRules.DotaPvP = DotaPvP()
-    GameRules.DotaPvP:InitGameMode()
+	GameRules.AddonTemplate = CAddonTemplateGameMode()
+	GameRules.AddonTemplate:InitGameMode()
 end
 
---------------------------------------------------------------------------------
--- INIT
---------------------------------------------------------------------------------
-function DotaPvP:InitGameMode()
-	local GameMode = GameRules:GetGameModeEntity()
-
-	-- Enable the standard Dota PvP game rules
-	GameRules:GetGameModeEntity():SetTowerBackdoorProtectionEnabled( true )
-
-	-- Register Think
-	GameMode:SetContextThink( "DotaPvP:GameThink", function() return self:GameThink() end, 0.25 )
-
-	-- Register Game Events
+function CAddonTemplateGameMode:InitGameMode()
+	print( "Template addon is loaded." )
+	GameRules:GetGameModeEntity():SetThink( "OnThink", self, "GlobalThink", 2 )
 end
 
---------------------------------------------------------------------------------
-function DotaPvP:GameThink()
-	return 0.25
+-- Evaluate the state of the game
+function CAddonTemplateGameMode:OnThink()
+	if GameRules:State_Get() == DOTA_GAMERULES_STATE_GAME_IN_PROGRESS then
+		--print( "Template addon script is running." )
+	elseif GameRules:State_Get() >= DOTA_GAMERULES_STATE_POST_GAME then
+		return nil
+	end
+	return 1
 end
