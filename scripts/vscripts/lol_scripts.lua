7e77ac011d8bc58266384d0437700dfa7aad5ab6
ColdAmphibian <ColdAmphibian@gmail.com>
2015-01-12 02:30:42 +1000

    Started trying to get towers to work properly, added other buildings too

--- scripts/vscripts/lol_scripts.lua
+++ scripts/vscripts/lol_scripts.lua
@@ -11,9 +11,13 @@ TowersBeforeInhibs = 3
 
 function RemoveInvincibility(entity)
 	--local name = entity:GetName()
-	Say(entity, "I Ran", false)
-	Say(entity, "I Ran", true)
+	print("top kek")
 	--print(name)
+
+	thisEntity:SetContextThink("printtopkek", function(entity)
+		print("top kek")
+		return 1
+		end, 0)
 end
 
 --RemoveSelfFromTable
@@ -23,6 +27,11 @@ end
 --=================================================================================
 
 BrushTable = {}
+
+function Test(trigger)
+	print(trigger.activator:GetName())
+	print("top kek")
+end
 
 function BrushStart(trigger)
 	local name = trigger.caller:GetEntityIndex()
@@ -87,7 +96,73 @@ function BrushEnd(trigger)
 	BrushTable[trigger.caller:GetEntityIndex()]["entities"][trigger.activator] = nil
 	--Say(brush, "You have entered: " .. "brush" .. trigger.caller:GetEntityIndex(), false)
 end
+
+BrushTable2 = {}
  
+function BrushStart2(trigger)
+	local name = trigger.caller:GetEntityIndex()
+	--Say(brush, "You have entered: " .. name, false)
+	if BrushTable2[name] == nil then
+		BrushTable2[name] = {["entities"] = {}, ["revealers"] = {}} 					
+	end
+
+	if trigger.activator:GetUnitName() == "npc_lol_brush_vision" then 				
+		--print(trigger.activator:GetUnitName())									
+		BrushTable2[name]["revealers"][trigger.activator] = true 					
+		--print(trigger.activator:GetUnitName())
+	else
+		--print(trigger.activator:GetName())
+		if trigger.activator:GetUnitName() ~= "npc_dota_courier" then
+			giveUnitDataDrivenModifier(trigger.activator, trigger.activator, "modifier_lol_in_brush", -1)
+		end
+		BrushTable2[name]["entities"][trigger.activator] = true
+		--print("starting updating for " .. name)
+
+		thisEntity:SetContextThink("brush-thinker-for-" .. name, function(trigger)
+				-- loops through each revealer
+				for revealer, v in pairs(BrushTable2[name]["revealers"]) do 
+					local largest_vr = 0
+					-- This gets the largest vision the revealer should set its vision to								
+					for entity, v in pairs(BrushTable2[name]["entities"]) do
+						-- Checks if the entity is touching the brush
+						if trigger:IsTouching(entity) then
+							-- Only gets vision for things on correct team
+							if entity:GetTeam() == revealer:GetTeam() then
+								local entity_vr = entity:GetCurrentVisionRange()
+								local d = CalcDistanceBetweenEntityOBB(entity, revealer)
+								largest_vr = math.max(entity_vr - d, largest_vr)
+							end
+						-- Else: remove it from the table, this prevents the script from crashing when something dies in the brush
+						else
+							--print("entity removed")
+							BrushTable2[name]["entities"][entity] = nil
+						end
+					end
+					-- this changes vision
+					revealer:SetDayTimeVisionRange(largest_vr)
+					revealer:SetNightTimeVisionRange(largest_vr)
+				end
+
+				-- This will check if there are any entities remaining from your team inside the brush, if not. It stops the checking loop
+				for revealer, v in pairs(BrushTable2[name]["revealers"]) do
+					for entity, v in pairs(BrushTable2[name]["entities"]) do
+						-- if there's an entity of the team as the revealer
+						if entity:GetTeam() == revealer:GetTeam() then
+							return 0.05
+						end
+					end
+				end
+				return nil
+			end, 0)
+	end
+end
+ 
+function BrushEnd2(trigger)
+	trigger.activator:RemoveModifierByName("modifier_lol_in_brush")
+	BrushTable2[trigger.caller:GetEntityIndex()]["entities"][trigger.activator] = nil
+	--Say(brush, "You have entered: " .. "brush" .. trigger.caller:GetEntityIndex(), false)
+end
+
 --Stops units from getting stuck
 function AntiBlockStart(trigger)
 	if trigger.activator then
